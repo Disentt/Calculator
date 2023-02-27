6d1cf1e6b4210cb03b7d29b0ec704fda16084c65
Egor <126278403+Disentt@users.noreply.github.com>
2023-02-27 08:37:00 +0200

    Add files via upload

---
+++ ihdfsghduoghrdohgsdoghosgdoigodishgsoipuahgpoihaqsdgipohsioa[hgaopis.py
@@ -0,0 +1,12 @@
+import math 
+fox = int(input("What one number? ")) 
+dog = int(input("What one number? "))
+rain = input ("-+*")
+if rain == "+":
+    print(fox + dog)
+elif rain == "-":   
+    print(fox - dog)  
+elif rain == "*":
+    print(fox * dog)
+elif rain == "/":
+    print(fox / dog)
